8c838dda3942af7f8e02e333a99b89ca0748042f
PhantomARC <51982416+PhantomARC@users.noreply.github.com>
2020-09-11 20:28:25 -0700

    Added GameStages
    Added Title Screen Game Stage and transitions between scenes

---
+++ P2P Connect Test/Scripts/ResulltsOkButton.gd
@@ -0,0 +1,8 @@
+extends Button
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_Button_button_down():
+	get_tree().change_scene("res://Scenes/TitleScreen.tscn")
+	pass # Replace with function body.

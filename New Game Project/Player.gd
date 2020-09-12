638e7cf74bc14cd854c5c40ffb250da18562b9bc
BobWinston <68573633+BobWinston@users.noreply.github.com>
2020-09-11 18:55:05 -0700

    Moving player nodes+tileset into main project

---
+++ New Game Project/Player.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+onready var ray = $RayCast2D
+
+var tile_size = 64
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size) #Snap rounds the position to the nearest tile increment, and adding a half-tile amount makes sure the player is centered on the tile.
+	position += Vector2.ONE * tile_size/2
+
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	ray.cast_to = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		position += inputs[dir] * tile_size
+
+	
+	
+
+
+

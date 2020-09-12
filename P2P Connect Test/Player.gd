ccc846a56b83347e646ef7b9b2e8ce9f7f477fdb
BobWinston <68573633+BobWinston@users.noreply.github.com>
2020-09-11 19:18:56 -0700

    :^)

--- P2P Connect Test/Player.gd
+++ P2P Connect Test/Player.gd
@@ -2,17 +2,24 @@ extends Area2D
 
 onready var ray = $RayCast2D
 
+onready var tween = $Tween
+
+export var speed = 3
+
 var tile_size = 64
-var inputs = {"right": Vector2.RIGHT,
-			"left": Vector2.LEFT,
-			"up": Vector2.UP,
-			"down": Vector2.DOWN}
+
+var inputs = {"ui_right": Vector2.RIGHT,
+			"ui_left": Vector2.LEFT,
+			"ui_up": Vector2.UP,
+			"ui_down": Vector2.DOWN}
 
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size) #Snap rounds the position to the nearest tile increment, and adding a half-tile amount makes sure the player is centered on the tile.
 	position += Vector2.ONE * tile_size/2
 
 func _unhandled_input(event):
+	if tween.is_active():
+		return
 	for dir in inputs.keys():
 		if event.is_action_pressed(dir):
 			move(dir)
@@ -21,8 +28,12 @@ func move(dir):
 	ray.cast_to = inputs[dir] * tile_size
 	ray.force_raycast_update()
 	if !ray.is_colliding():
-		position += inputs[dir] * tile_size
+		#position += inputs[dir] * tile_size
+		 move_tween(dir)
 
+func move_tween(dir):
+	tween.interpolate_property(self, "position", position, position + dir * tile_size, 1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
 	
 	
 

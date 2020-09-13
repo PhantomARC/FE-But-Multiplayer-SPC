5862846e3f964712c744355e7e202966b612a22a
PhantomARC <51982416+PhantomARC@users.noreply.github.com>
2020-09-12 18:53:09 -0700

    Added some code to MapSelect
    Fixed A few bugs to loda scene

--- P2P Connect Test/Scripts/MainScene.gd
+++ P2P Connect Test/Scripts/MainScene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var selectMap = null
 
 func go_to_red_team_end_screen():
 	get_tree().change_scene("res://Scenes/RedTeamEndScreen.tscn")
@@ -11,9 +11,11 @@ func go_to_blue_team_end_screen():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if $global.mapSelect == 1:
-		add_child_below_node(MainScene, MapVolcano.tscn, false)
-	if $global.mapSelect == 2:
-		add_child_below_node(MainScene, MapPlains.tscn, false)
+		selectMap = load("res://Sprites/MapVolcano.tscn")
+	elif $global.mapSelect == 2:
+		selectMap = load("res://Sprites/MapPlains.tscn")
+	var loadMap = selectMap.instance()
+	add_child(loadMap)
 
 	pass # Replace with function body.
 

2b5d8965d596795972a88a6e34132b473c6404ce
PhantomARC <51982416+PhantomARC@users.noreply.github.com>
2020-09-13 22:11:21 -0700

    Update Camera2D.gd
    Simplified GDScript to be less Vector2() and more mathematically simplified

--- P2P Connect Test/Scripts/Camera2D.gd
+++ P2P Connect Test/Scripts/Camera2D.gd
@@ -1,24 +1,26 @@
 extends Camera2D
 
-var zoom_states = [Vector2(1.5,1.5),Vector2(1.25,1.25),Vector2(1,1),Vector2(0.75,0.75),Vector2(0.5,0.5),Vector2(0.25,0.25)]
-var current_zoom = get_zoom()
-var zoom_main = 2
-const ZOOM_SPEED = 5
 
+var zoom_calc = 1
+var current_zoom = get_zoom()
+const ZOOM_SPEED = 5
 
 
 func _ready():
 	pass
 
-func _process(delta):
-	if (Input.is_action_just_released("scroll_up")):
-		zoom_main += 1
-		zoom_main = min(zoom_main, 5)
 
+func _process(delta):
 	if (Input.is_action_just_released("scroll_down")):
-		zoom_main -= 1
-		zoom_main = max(zoom_main, 0)
+		zoom_calc += 0.25
+		zoom_calc = min(zoom_calc, 1.5)
+
+
+	if (Input.is_action_just_released("scroll_up")):
+		zoom_calc -= 0.25
+		zoom_calc = max(zoom_calc, 0.25)
 	
-	if (current_zoom != zoom_states[zoom_main]):
-		current_zoom = lerp(get_zoom(), zoom_states[zoom_main], ZOOM_SPEED * delta)
+	
+	if (current_zoom != Vector2(zoom_calc, zoom_calc)):
+		current_zoom = lerp(get_zoom(), Vector2(zoom_calc, zoom_calc), ZOOM_SPEED * delta)
 		set_zoom(current_zoom)

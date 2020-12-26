60c8fb8d1b233478098722a9e90c67945129958a
PhantomARC <51982416+PhantomARC@users.noreply.github.com>
2020-12-26 01:38:53 -0800

    personal preference changes
    Raw commit to vol and transparency. Will make dat file soon.

--- P2P Connect Test/Scripts/global.gd
+++ P2P Connect Test/Scripts/global.gd
@@ -4,8 +4,8 @@ extends Node
 var other_id = -1 #default other player IDs' to -1
 var map_select = 1 #default to Map 1 on load
 var igt_turn = true #true = blue, false = red
-var volume = -80
-var transparency = 0.5
+var volume = -10
+var transparency = 0.8
 
 func set_map_select(map_select_number): #when called, change map
 	map_select = map_select_number
